3251f118f1286e26d2aa754f08ad2ed99b2f5fb6
bouzuya <m@bouzuya.net>
2026-02-27 22:57:41 +0900

    Add Firestore struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod error;
+mod firestore;
 mod firestore_client;
 
 pub use self::error::Error;
+pub use self::firestore::Firestore;
 pub use self::firestore_client::FirestoreClient;
---
+++ tests/v0_1/firestore.rs
@@ -0,0 +1,5 @@
+#[test]
+fn test_firestore_import() {
+    use bouzuya_firestore_client::Firestore;
+    let _: Option<Firestore> = None;
+}
