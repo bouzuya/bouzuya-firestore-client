b6076d97b6790aea0a32e9f566555af3632c9d6d
bouzuya <m@bouzuya.net>
2026-02-28 22:06:05 +0900

    Add CollectionReference struct

---
+++ src/collection_reference.rs
@@ -0,0 +1,3 @@
+pub struct CollectionReference {
+    _private: (),
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+mod collection_reference;
 mod error;
 mod firestore;
 mod firestore_client;
 
+pub use self::collection_reference::CollectionReference;
 pub use self::error::Error;
 pub use self::firestore::Firestore;
 pub use self::firestore_client::FirestoreClient;
