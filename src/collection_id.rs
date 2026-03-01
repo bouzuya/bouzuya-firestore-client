8a88cd6b7119fac4d7c2ed558426f71fea97718d
bouzuya <m@bouzuya.net>
2026-03-01 17:35:39 +0900

    Add pub(crate) CollectionId::from_collection_id

--- src/collection_id.rs
+++ src/collection_id.rs
@@ -12,6 +12,12 @@ impl From<E> for Error {
 
 pub struct CollectionId(firestore_path::CollectionId);
 
+impl CollectionId {
+    pub(crate) fn from_collection_id(collection_id: firestore_path::CollectionId) -> Self {
+        Self(collection_id)
+    }
+}
+
 impl std::fmt::Display for CollectionId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.0.fmt(f)
@@ -26,5 +32,18 @@ impl std::str::FromStr for CollectionId {
             .map(Self)
             .map_err(E)
             .map_err(Error::from)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_from_collection_id() -> anyhow::Result<()> {
+        use crate::collection_id::CollectionId;
+        use std::str::FromStr as _;
+        let inner = firestore_path::CollectionId::from_str("rooms")?;
+        let collection_id = CollectionId::from_collection_id(inner);
+        assert_eq!(collection_id.to_string(), "rooms");
+        Ok(())
     }
 }
