48bc3f7c6f497425268fcede9ce55a860db3715d
bouzuya <m@bouzuya.net>
2026-03-10 22:27:26 +0900

    Add tests for TransactionOptions

--- tests/v0_1/transaction_options.rs
+++ tests/v0_1/transaction_options.rs
@@ -3,3 +3,38 @@ fn test_transaction_options_import() {
     use bouzuya_firestore_client::TransactionOptions;
     let _: Option<TransactionOptions> = None;
 }
+
+#[test]
+fn test_transaction_options_max_attempts() {
+    use bouzuya_firestore_client::TransactionOptions;
+    let options = TransactionOptions {
+        max_attempts: Some(5),
+        read_only: None,
+        read_time: None,
+    };
+    assert_eq!(options.max_attempts, Some(5));
+}
+
+#[test]
+fn test_transaction_options_read_only() {
+    use bouzuya_firestore_client::TransactionOptions;
+    let options = TransactionOptions {
+        max_attempts: None,
+        read_only: Some(true),
+        read_time: None,
+    };
+    assert_eq!(options.read_only, Some(true));
+}
+
+#[test]
+fn test_transaction_options_read_time() {
+    use bouzuya_firestore_client::Timestamp;
+    use bouzuya_firestore_client::TransactionOptions;
+    let read_time = Timestamp::from_millis(1_000);
+    let options = TransactionOptions {
+        max_attempts: None,
+        read_only: None,
+        read_time: Some(read_time),
+    };
+    assert_eq!(options.read_time, Some(read_time));
+}
