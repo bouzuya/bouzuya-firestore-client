d1990b854750d0fc1fbf81d0dbfb827fc2c7f068
bouzuya <m@bouzuya.net>
2026-03-12 21:03:45 +0900

    Add impl Default for Precondition

--- src/precondition.rs
+++ src/precondition.rs
@@ -1,5 +1,6 @@
 use crate::Timestamp;
 
+#[derive(Default)]
 pub struct Precondition {
     pub exists: Option<bool>,
     pub last_update_time: Option<Timestamp>,
--- tests/v0_1/precondition.rs
+++ tests/v0_1/precondition.rs
@@ -1,4 +1,12 @@
 #[test]
+fn test_default() {
+    use bouzuya_firestore_client::Precondition;
+    let precondition = Precondition::default();
+    assert!(precondition.exists.is_none());
+    assert!(precondition.last_update_time.is_none());
+}
+
+#[test]
 fn test_precondition_import() {
     use bouzuya_firestore_client::Precondition;
     let _: Option<Precondition> = None;
