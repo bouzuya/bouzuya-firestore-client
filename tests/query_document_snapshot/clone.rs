be674d2594c30ba2347185f683431b97a729cb65
bouzuya <m@bouzuya.net>
2026-04-21 21:18:34 +0900

    Fix test for `impl Clone for QueryDocumentSnapshot`

--- tests/query_document_snapshot/clone.rs
+++ tests/query_document_snapshot/clone.rs
@@ -1,9 +1,26 @@
 // since v2.1
-#[test]
-fn test_query_document_snapshot_clone() {
-    // FIXME: add tests when QueryDocumentSnapshot can be obtained via the public Query API
-    let _: fn(
-        &bouzuya_firestore_client::QueryDocumentSnapshot,
-    ) -> bouzuya_firestore_client::QueryDocumentSnapshot =
-        bouzuya_firestore_client::QueryDocumentSnapshot::clone;
+#[tokio::test]
+#[serial_test::serial]
+async fn test_query_document_snapshot_clone() -> anyhow::Result<()> {
+    use bouzuya_firestore_client::Firestore;
+    use bouzuya_firestore_client::FirestoreOptions;
+    use bouzuya_firestore_client::QueryDocumentSnapshot;
+    use std::collections::HashMap;
+
+    fn assert_impl<T: Clone>() {}
+    assert_impl::<QueryDocumentSnapshot>();
+
+    let firestore = Firestore::new(FirestoreOptions::default())?;
+    let id = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)?
+        .as_nanos()
+        .to_string();
+    let collection_ref = firestore.collection(format!("rooms/{}/items", id))?;
+    collection_ref.add(HashMap::<String, String>::new()).await?;
+    let query_snapshot = collection_ref.get().await?;
+    let docs = query_snapshot.docs();
+    assert_eq!(docs.len(), 1);
+    let cloned = docs[0].clone();
+    assert_eq!(cloned.id(), docs[0].id());
+    Ok(())
 }
