aafbd5017807db1b2fd7c4cb03cfaddf307937e2
bouzuya <m@bouzuya.net>
2026-04-21 20:36:13 +0900

    Fix test for QuerySnapshot::docs

--- tests/query_snapshot/docs.rs
+++ tests/query_snapshot/docs.rs
@@ -1,9 +1,18 @@
 // since v2.1
-#[test]
-fn test_query_snapshot_docs() {
-    // FIXME: add tests when QuerySnapshot can be constructed via the public Query API
-    let _: fn(
-        &bouzuya_firestore_client::QuerySnapshot,
-    ) -> Vec<bouzuya_firestore_client::QueryDocumentSnapshot> =
-        bouzuya_firestore_client::QuerySnapshot::docs;
+#[tokio::test]
+#[serial_test::serial]
+async fn test_query_snapshot_docs() -> Result<(), bouzuya_firestore_client::Error> {
+    use bouzuya_firestore_client::Firestore;
+    use bouzuya_firestore_client::FirestoreOptions;
+    use std::collections::HashMap;
+    let firestore = Firestore::new(FirestoreOptions::default())?;
+    let collection_ref = firestore.collection("rooms")?;
+    collection_ref.add(HashMap::<String, String>::new()).await?;
+    let query_snapshot = collection_ref.get().await?;
+    let docs = query_snapshot.docs();
+    assert!(!docs.is_empty());
+    for doc in docs {
+        assert!(doc.exists());
+    }
+    Ok(())
 }
