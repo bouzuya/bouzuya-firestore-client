04d50d0e0d3f3259205daa1af8f69e95a87d05e9
bouzuya <m@bouzuya.net>
2026-04-21 20:42:48 +0900

    Fix test for QueryDocumentSnapshot::data

--- tests/query_document_snapshot/data.rs
+++ tests/query_document_snapshot/data.rs
@@ -1,6 +1,23 @@
 // since v2.1
-
-#[test]
-fn test_query_document_snapshot_data() {
-    // FIXME: add tests when QueryDocumentSnapshot can be obtained via the public Query API
+#[tokio::test]
+#[serial_test::serial]
+async fn test_query_document_snapshot_data() -> anyhow::Result<()> {
+    use bouzuya_firestore_client::Firestore;
+    use bouzuya_firestore_client::FirestoreOptions;
+    use std::collections::HashMap;
+    let firestore = Firestore::new(FirestoreOptions::default())?;
+    let id = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)?
+        .as_nanos()
+        .to_string();
+    let collection_ref = firestore.collection(format!("rooms/{}/items", id))?;
+    let data = HashMap::from([("key".to_string(), "value".to_string())]);
+    collection_ref.add(data).await?;
+    let query_snapshot = collection_ref.get().await?;
+    let docs = query_snapshot.docs();
+    assert_eq!(docs.len(), 1);
+    let doc = &docs[0];
+    let got: HashMap<String, String> = doc.data()?;
+    assert_eq!(got.get("key").map(String::as_str), Some("value"));
+    Ok(())
 }
