5251edd7e3b616dfc16e8ae79a301f5561af2940
bouzuya <m@bouzuya.net>
2026-05-05 20:09:52 +0900

    Add pub(crate) CollectionGroup::new

--- src/collection_group.rs
+++ src/collection_group.rs
@@ -1,3 +1,33 @@
+use crate::Firestore;
+
 pub struct CollectionGroup {
-    _private: (),
+    #[allow(dead_code)]
+    collection_id: firestore_path::CollectionId,
+    #[allow(dead_code)]
+    firestore: Firestore,
+}
+
+impl CollectionGroup {
+    pub(crate) fn new(collection_id: firestore_path::CollectionId, firestore: Firestore) -> Self {
+        Self {
+            collection_id,
+            firestore,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[tokio::test]
+    async fn test_new() -> anyhow::Result<()> {
+        use crate::CollectionGroup;
+        use crate::Firestore;
+        use crate::FirestoreOptions;
+        use std::str::FromStr as _;
+        let collection_id = firestore_path::CollectionId::from_str("rooms")?;
+        let firestore = Firestore::new(FirestoreOptions::default())?;
+        let _collection_group = CollectionGroup::new(collection_id, firestore);
+        // NOTE: can't test CollectionGroup::collection_id() because it's private
+        Ok(())
+    }
 }
